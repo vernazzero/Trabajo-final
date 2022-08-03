19152d9815150b7748a1b24d9e2f87f691b3c53d
Elias <EEliasCarabajal@gmail.com>
2022-08-03 19:32:37 -0300

    iniciando en db

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { useState, useEffect } from "react";
 import Header from './componentes/Header';
 import Hero from './componentes/Hero';
 import About from './componentes/About';
 import Caracteristicas from './componentes/Caracteristicas';
 import Fin from './componentes/Fin';
 import Footer from './componentes/Footer';
+import { obtenerProductos } from './FireBase';
 
 function App() {
+
+  const [productos, setProductos] = useState([]);
+
+  useEffect(()=>{
+
+    obtenerProductos()
+
+  },[])
 
   return(
 
